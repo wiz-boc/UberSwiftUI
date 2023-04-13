d0cad92505a26c3dfd82870ab34b926a29002589
wiz-boc <wiz_kidz@outlook.com>
2023-04-12 22:12:20 -0500

    Adding annotations to mapview

--- UberSwiftUI/Core/Home/Views/UberMapViewRepresentable.swift
+++ UberSwiftUI/Core/Home/Views/UberMapViewRepresentable.swift
@@ -25,7 +25,7 @@ struct UberMapViewRepresentable: UIViewRepresentable {
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
         if let coordinate = locationViewModel.selectedLocationCoordinate {
-            
+            context.coordinator.addAndSelectAnnotation(with: coordinate)
         }
     }
     
@@ -47,6 +47,18 @@ extension UberMapViewRepresentable {
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
             parent.mapView.setRegion(region, animated: true)
+        }
+        
+        //MARK: - Helpers
+        
+        func addAndSelectAnnotation(with coordinate: CLLocationCoordinate2D){
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            
+            let anno = MKPointAnnotation()
+            anno.coordinate = coordinate
+            parent.mapView.addAnnotation(anno)
+            parent.mapView.selectAnnotation(anno, animated: true)
+            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
         }
     }
 }
