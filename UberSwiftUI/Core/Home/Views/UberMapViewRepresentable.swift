932e54bb241828a6d889555d1ee090a461e4883c
wiz-boc <wiz_kidz@outlook.com>
2023-04-13 22:26:17 -0500

    Clean up polylines

--- UberSwiftUI/Core/Home/Views/UberMapViewRepresentable.swift
+++ UberSwiftUI/Core/Home/Views/UberMapViewRepresentable.swift
@@ -11,6 +11,7 @@ import MapKit
 struct UberMapViewRepresentable: UIViewRepresentable {
     let mapView = MKMapView()
     let locationManager = LocationManager()
+    @Binding var mapState: MapViewState
     @EnvironmentObject var locationViewModel: LocationSearchViewModel
     
     func makeUIView(context: Context) -> some UIView {
@@ -24,9 +25,20 @@ struct UberMapViewRepresentable: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        if let coordinate = locationViewModel.selectedLocationCoordinate {
-            context.coordinator.addAndSelectAnnotation(with: coordinate)
-            context.coordinator.configurePolyline(with: coordinate)
+        print("DEBUG: Map State is \(mapState)")
+        
+        switch mapState {
+            case .noInput:
+                context.coordinator.clearMapViewAndRecenterOnUserLocation()
+                break
+            case .searchingForLocation:
+                break
+            case .locationSelected:
+                if let coordinate = locationViewModel.selectedLocationCoordinate {
+                    context.coordinator.addAndSelectAnnotation(with: coordinate)
+                    context.coordinator.configurePolyline(with: coordinate)
+                }
+                break
         }
     }
     
@@ -40,6 +52,8 @@ extension UberMapViewRepresentable {
     class MapCoordinator: NSObject, MKMapViewDelegate {
         let parent: UberMapViewRepresentable
         var userLocationCoordinate: CLLocationCoordinate2D?
+        var currentRegion: MKCoordinateRegion?
+        
         init(parent: UberMapViewRepresentable) {
             self.parent = parent
             super.init()
@@ -49,6 +63,7 @@ extension UberMapViewRepresentable {
             userLocationCoordinate = userLocation.coordinate
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
             parent.mapView.setRegion(region, animated: true)
+            currentRegion = region
         }
         
         func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
@@ -88,12 +103,20 @@ extension UberMapViewRepresentable {
             
             directions.calculate { response, error in
                 if let error = error {
-                    print("DEBUG : Failed to get directions with error")
+                    print("DEBUG : Failed to get directions with error \(error)")
                     return
                 }
                 
                 guard let route = response?.routes.first else { return }
                 completion(route)
+            }
+        }
+        
+        func clearMapViewAndRecenterOnUserLocation(){
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            parent.mapView.removeOverlays(parent.mapView.overlays)
+            if let currentRegion = currentRegion {
+                parent.mapView.setRegion(currentRegion, animated: true)
             }
         }
     }
---
+++ UberSwiftUI/Models/MapViewState.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewState.swift
+//  UberSwiftUI
+//
+//  Created by wizz on 4/13/23.
+//
+
+import Foundation
+
+enum MapViewState {
+    case noInput
+    case searchingForLocation
+    case locationSelected
+    
+}
